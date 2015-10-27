8e8d10dfee7ba9cc3400630bd25af154d0eb590b
Kaboose <kevintjet@hotmail.com>
2015-10-27 11:59:23 +0100

    Project prep

---
+++ src/main/java/com/kevincyt/toskeys/gui/main/MainPresenter.java
@@ -0,0 +1,15 @@
+package com.kevincyt.toskeys.gui.main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class MainPresenter implements Initializable{
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+
+}
---
+++ src/main/java/com/kevincyt/toskeys/gui/main/MainView.java
@@ -0,0 +1,7 @@
+package com.kevincyt.toskeys.gui.main;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+public class MainView extends FXMLView {
+
+}
